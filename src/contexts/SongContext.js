7b9b479962afabdcd210847733e0d44aaa7b1b8d
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2019-12-20 01:18:44 -0300

    useEffect para localStorage

--- src/contexts/SongContext.js
+++ src/contexts/SongContext.js
@@ -1,11 +1,18 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 
 import { songReducer } from "../reducers/songReducer";
 
 export const SongContext = createContext();
 
 const SongContextProvider = props => {
-  const [songs, dispatch] = useReducer(songReducer, []);
+  const [songs, dispatch] = useReducer(songReducer, [], () => {
+    const localData = localStorage.getItem("songs");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("songs", JSON.stringify(songs));
+  }, [songs]);
 
   return (
     <SongContext.Provider value={{ songs, dispatch }}>
